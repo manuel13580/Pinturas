9e233e7718f93c21708aa939cc2fb7743f56383a
Angel <angelgbriel@gmail.com>
2017-08-22 23:20:03 -0600

    Se completo la creacion de  tintes

--- Pinturas/Tintes.cs
+++ Pinturas/Tintes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,45 @@ namespace Pinturas
 {
     public partial class Tintes : Form
     {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
         public Tintes()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool registro = registrar_Tinte(Convert.ToString(txtcodigo.Text), Convert.ToString(txtnombre.Text),
+                                           Convert.ToString(txtmonocapa.Text), Convert.ToInt32(textrgb.Text));
+        }
+        public bool registrar_Tinte(string codigo, string nombre, string mono_capa, int rgb)
+        {
+
+            try
+
+            {
+
+                cmd = new SqlCommand("INSERT INTO TINTE(Codigo,Nombre,Mono_Capa,Rgb)" +
+                                     " values('" + codigo + "','" + nombre + "','" + mono_capa + "'," + rgb + ")", cn);
+                cmd.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                return true;
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("No se inserto Por :  " + ex.ToString());
+                return false;
+
+            }
+
+        }
+
     }
 }
