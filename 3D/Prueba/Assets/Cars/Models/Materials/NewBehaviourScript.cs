d5c45acf103534ae4cee94e6b05fdcc6910a4873
Manuel <manuel13580@gmail.com>
2017-10-26 14:05:29 -0600

    Parte 3d

---
+++ 3D/Prueba/Assets/Cars/Models/Materials/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Material colorInicial;
+	// Use this for initialization
+	void Start()
+    {
+        GetComponent<Renderer>().material = colorInicial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorInicial.color = new UnityEngine.Color(0,0,0);
+    }
+}
